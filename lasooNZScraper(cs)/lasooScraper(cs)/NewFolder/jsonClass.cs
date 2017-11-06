fe8e4a964faa2ce0f03a2c47f0b48f2628f812ec
xiaohan <32392745+saeedmohamed451@users.noreply.github.com>
2017-11-06 12:22:34 +0800

    original

---
+++ lasooNZScraper(cs)/lasooScraper(cs)/NewFolder/jsonClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lasooScraper_cs_.NewFolder
+{
+    public class jsonClass
+    {
+        public string pageNo { get; set; }
+        public string image { get; set; }
+        public List<offer> offers { get; set; }
+    }
+}
